1c5f23f4ba07ab24cad17962f9db299c638b4bee
justin nelson <Jt.ermn@gmail.com>
2020-11-13 18:13:54 -0600

    this is a test in edits

---
+++ LogIn.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace AeroMaterialHandlingDatabaseApplication
+{
+    public partial class fLogIn : Form
+    {
+        public fLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void btLoginExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btlogin_Click(object sender, EventArgs e)
+        {
+            string userName = tbUserNameLogIn.Text.ToUpper(); 
+            string password = tbPasswordLogIn.Text;
+            string holderpw = "";
+            string holderun = "";
+
+            lblUserErrorLogIn.Visible = false;
+            lblPasswordErrorLogIn.Visible = false;
+            if (userName!=holderun)
+            {
+                lblUserErrorLogIn.Text = "Error: User Name is Incorrect";
+                lblUserErrorLogIn.Visible = true;
+                tbUserNameLogIn.Text = "";
+                lblPasswordErrorLogIn.Text = "Error: User Name Error";
+                lblPasswordErrorLogIn.Visible = true;
+                tbPasswordLogIn.Text = "";
+            }
+
+            else if (password!=holderpw)
+            {
+                lblPasswordErrorLogIn.Text = "Error: Password is Incorrect";
+                lblPasswordErrorLogIn.Visible = true;
+                tbPasswordLogIn.Text = "";
+            }
+            else
+            {
+
+                //Close LogIn,the current open form.
+                this.Hide();
+                //Opens ViewPage
+                fViewPage f = new fViewPage();
+                f.ShowDialog();
+                
+
+            }
+        }
+
+        private void lblUserError_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fLogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ViewPage.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AeroMaterialHandlingDatabaseApplication
+{
+    public partial class fViewPage : Form
+    {
+        public fViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewPage_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void scDivide_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void btNewEntry_Click(object sender, EventArgs e)
+        {
+            //Opens EditPage
+            fEditPage f = new fEditPage();
+            f.ShowDialog();
+        }
+
+        private void pbUser_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pnSearchResult_MouseClick(object sender, MouseEventArgs e)
+        {
+            //This Code will set the splitter distance split panal2
+            scDivide.SplitterDistance = 200;
+        }
+
+        private void flowLayoutPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            //This Code will set the splitter distance to focus on split panal1
+            scDivide.SplitterDistance = 900;
+        }
+        private void scDivide_Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //This Code will set the splitter distance to focus on split panal1
+            scDivide.SplitterDistance = 900;
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            //automatically start populating the flow planel
+            populateItems();
+        }
+        
+        
+        //Poplulate the flowpanel left side
+        private void populateItems()
+        {
+            ListItem[] listItems = new ListItem[20];
+
+            for (int i = 0; i < listItems.Length; i++)
+            {
+                listItems[i] = new ListItem();
+                listItems[i].Title = "Title -- Lorum Ipsum";
+                listItems[i].Tags = "Tags";
+                listItems[i].shortDesc = "This is a short descriptino of the product -- Lorum Ipsum";
+
+                if (flp1.Controls.Count < 0)
+                {
+                    flp1.Controls.Clear();
+                }
+                else
+                    flp1.Controls.Add(listItems[i]);
+
+            }
+        }
+
+
+    }
+}
